458b29f32340b0672e9d75daadb21dace4174538
Hattivat <jan.janiczek@yahoo.com>
2016-06-18 02:01:22 +0200

    Final exercises of week 3 done

--- UCB_exercises.scm
+++ UCB_exercises.scm
@@ -73,3 +73,32 @@
   (if (= n (sum-of-factors 0 n 1))
       n
       (next-perf (+ n 1))))
+
+(define (number-of-partitions n)
+  (define (nop n x)
+    (cond ((= n 0) 1)
+          ((or(< n 0) (= x 0)) 0)
+          (else (+ (nop n (- x 1)) (nop (- n x) x)))))
+  (nop n n))
+
+(define (count-change amount)
+  (cc amount '(50 25 10 5 1)))
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (empty? kinds-of-coins)) 0)
+        (else (+ (cc amount
+                     (bf kinds-of-coins))
+                 (cc (- amount
+                        (first kinds-of-coins))
+                     kinds-of-coins)))))
+
+(define (type-check func test x)
+  (if (test x)
+      (func x)
+      #f))
+
+(define (make-safe func test)
+  (lambda (x) (if (test x)
+                  (func x)
+                  #f)))
+
