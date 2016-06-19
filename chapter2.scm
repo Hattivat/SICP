3636449152da80d523a2ec25f3c0fa80adaf8f23
Hattivat <jan.janiczek@yahoo.com>
2016-06-19 21:03:10 +0200

    beginning chapter 2

---
+++ chapter2.scm
@@ -0,0 +1,45 @@
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (cons (/ n g) (/ d g))))
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+(define (print-rat x)
+  (display (numer x))
+  (display '/)
+  (display (denom x))
+  (newline))
+
+;exercise 2.1
+(define (better-make-rat n d)
+  (let ((g (gcd n d)))
+    (if (or (and (< n 0) (< d 0)) (and (> n 0) (< d 0)))
+        (cons (/ (* n -1) g) (/ (* d -1) g)))
+        (cons (/ n g) (/ d g))))
