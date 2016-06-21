30aad9c6ad680ad03f03965491616e368c2938b7
Hattivat <jan.janiczek@yahoo.com>
2016-06-21 02:59:19 +0200

    section 2.1 almost done

--- chapter2.scm
+++ chapter2.scm
@@ -102,4 +102,95 @@
 (define (mad-cdr x)
   (extract-exponent x 3 0))
 
+(define zero (lambda (f) (lambda (x) x)))
+
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+
 ;exercise 2.6
+(define one (lambda (f) (lambda (x) (f x))))
+(define two (lambda (f) (lambda (x) (f (f x)))))
+(define (plus m n)
+  (lambda (f) (lambda (x) ((m f) ((n f) x)))))
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (mul-interval x
+                (make-interval (/ 1.0 (upper-bound y))
+                               (/ 1.0 (lower-bound y)))))
+
+(define (make-interval a b)
+  (cons a b))
+
+;exercise 2.7
+(define (lower-bound interval)
+  (car interval))
+(define (upper-bound interval)
+  (cdr interval))
+
+;exercise 2.8
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (ipper-bound x) (lower-bound y))))
+
+;exercise 2.9
+(define (width-interval interval)
+  (/ (- (upper-bound interval) (lower-bound interval)) 2.0))
+
+;exercise 2.10
+(define (div-interval-upg x y)
+  (if (or (= 0 (lower-bound y)) (= 0 (upper-bound y)))
+      (display 'Error!)
+      (mul-interval x
+                    (make-interval (/ 1.0 (upper-bound y))
+                                   (/ 1.0 (lower-bound y))))))
+
+;exercise 2.11
+(define (mul-interval-ben x y)
+    (cond ((and (< (lower-bound x) 0) (< (upper-bound x) 0) (< (lower-bound y) 0) (< (upper-bound y) 0))
+           (make-interval (* (upper-bound x) (upper-bound y)) (* (lower-bound x) (lower-bound y))))
+          ((and (< (lower-bound x) 0) (< (upper-bound x) 0) (< (lower-bound y) 0))
+           (make-interval (* (lower-bound x) (upper-bound y)) (* (lower-bound x) (lower-bound y))))
+          ((and (< (lower-bound x) 0) (< (upper-bound x) 0))
+           (make-interval (* (upper-bound x) (lower-bound y)) (* (lower-bound x) (upper-bound y))))
+          ((and (< (lower-bound x) 0) (< (lower-bound y) 0) (< (upper-bound y) 0))
+           (make-interval (* (upper-bound x) (lower-bound y)) (* (lower-bound x) (lower-bound y))))
+          ((and (< (lower-bound x) 0) (< (lower-bound y) 0))
+           (make-interval (min (* (lower-bound x) (upper-bound y)) (* (upper-bound x) (lower-bound y)))
+                          (max (* (lower-bound x) (lower-bound y)) (* (upper-bound x) (upper-bound y)))))
+          ((< (lower-bound x) 0)
+           (make-interval (* (lower-bound x) (upper-bound y)) (* (upper-bound x) (upper-bound y))))
+          ((and (< (lower-bound y) 0) (< (upper-bound y) 0))
+           (make-interval (* (lower-bound x) (upper-bound y)) (* (upper-bound x) (lower-bound y))))
+          ((< (lower-bound y) 0)
+           (make-interval (* (upper-bound x) (lower-bound y)) (* (upper-bound x) (upper-bound y))))
+          (else (make-interval (* (lower-bound x) (lower-bound y)) (* (upper-bound x) (upper-bound y))))))
+
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+;exercise 2.12
+(define (make-center-percent center percent)
+  (let ((tolerance (/ percent 100.0)))
+  (make-center-width center (* center tolerance))))
+(define (percent interval)
+  (* 100 (/ (- (upper-bound interval) (lower-bound interval) 2.0))))
+
+;exercise 2.13
