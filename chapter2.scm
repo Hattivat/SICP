3f1f6aafd1b3e00aeb53fbd4033a87e9261395b8
Hattivat <jan.janiczek@yahoo.com>
2016-08-11 00:58:15 +0200

    beginning section 2.3

--- chapter2.scm
+++ chapter2.scm
@@ -537,3 +537,17 @@
 ;It is O(n^n) instead of O(n^2), so it will execute in T^(n-2)
 
 ;section 2.2.4 in "project2.scm"
+
+(define (memq item x)
+  (cond ((null? x) false)
+        ((eq? item (car x)) x)
+        (else (memq item (cdr x)))))
+
+;exercise 2.53
+;'(a b c)
+;'((george))
+;'((y1 y2))
+;'(y1 y2)
+;#f
+;#f
+;#t
