c8c38467a3acd61ec59f9ed21ca5e518fc8ebfd7
Hattivat <jan.janiczek@yahoo.com>
2016-08-16 01:38:18 +0200

    exercise 2.59 done

--- chapter2.scm
+++ chapter2.scm
@@ -669,8 +669,8 @@
 (define (augend2 s) (caddr p))
 
 (define (element-of-set? x set)
-  (cond ((null? set) false)
-        ((equal? x (car set)) true)
+  (cond ((null? set) #f)
+        ((equal? x (car set)) #t)
         (else (element-of-set? x (cdr set)))))
 
 (define (adjoin-set x set)
