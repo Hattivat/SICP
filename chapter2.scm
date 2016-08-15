ac7ae70656abe4c960a14c22affdfce30e334ad5
Hattivat <jan.janiczek@yahoo.com>
2016-08-15 18:54:07 +0200

    exercise 2.57 almost working

--- chapter2.scm
+++ chapter2.scm
@@ -570,35 +570,14 @@
 (define (same-variable? v1 v2)
   (and (variable? v1) (variable? v2) (eq? v1 v2)))
 
-(define (make-sum a1 a2)
-  (cond ((=number? a1 0) a2)
-        ((=number? a2 0) a1)
-        ((and (number? a1) (number? a2)) (+ a1 a2))
-        (else (list '+ a1 a2))))
-
 (define (=number? exp num)
   (and (number? exp) (= exp num)))
-
-(define (make-product m1 m2)
-  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
-        ((=number? m1 1) m2)
-        ((=number? m2 1) m1)
-        ((and (number? m1) (number? m2)) (* m1 m2))
-        (else (list '* m1 m2))))
 
 (define (sum? x)
   (and (pair? x) (eq? (car x) '+)))
 
-(define (addend s) (cadr s))
-
-(define (augend s) (caddr s))
-
 (define (product? x)
   (and (pair? x) (eq? (car x) '*)))
-
-(define (multiplier p) (cadr p))
-
-(define (multiplicand p) (caddr p))
 
 ;exercise 2.56
 (define (deriv exp var)
@@ -627,6 +606,35 @@
   (cond ((=number? e 0) 1)
         ((=number? e 1) b)
         ((=number? b 1) 1)
+        ((and (number? base) (number? exponent)) (expt base exponent))
         (else (list '** b e))))
 (define (exponentiation? x)
   (and (pair? x) (eq? (car x) '**)))
+
+;exercise 2.57
+(define (not-number? x)
+  (not (number? x)))
+(define (is-zero? x)
+  (=number? x 0))
+(define (make-sum . args)
+  (let ((result (cons (accumulate + (filter number? args))
+                      (filter not-number? args))))
+    (if (= 1 (length result))
+        (car result)
+        (cons '+ result))))
+(define (addend s) (cadr s))
+(define (augend s) (if (null? (cdddr s))
+                       (caddr s)
+                       (make-sum (cddr s))))
+(define (make-product . args)
+  (if (empty? (filter is-zero? args))
+      (let ((result (cons (accumulate * (filter number? args))
+                          (filter not-number? args))))
+        (if (= 1 (length result))
+            (car result)
+            (cons '* result)))
+      0))
+(define (multiplier p) (cadr p))
+(define (multiplicand p) (if (null? (cdddr p))
+                             (caddr p)
+                             (make-product (cddr p))))
