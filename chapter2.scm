ff6a61c8eb0318aecad3e6c2789fa53169bc00f2
Hattivat <jan.janiczek@yahoo.com>
2016-08-24 01:41:30 +0200

    more supplementary code from the book

--- chapter2.scm
+++ chapter2.scm
@@ -797,3 +797,22 @@
 ;exercise 2.63
 ;The two procedures produce the same results.
 ;The first procedure is O(n*logn), the second is O(n)
+
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result (partial-tree (cdr non-left-elts)
+                                              right-size)))
+              (let ((right-tree (car right-result))
+                    (remaining-elts (cdr right-result)))
+                (cons (make-tree this-entry left-tree right-tree)
+                      remaining-elts))))))))
