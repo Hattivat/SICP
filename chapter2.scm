47ac2d8b3e8b571a39b9de04336ae0cce7257fe4
Jan Janiczek <jan.janiczek@yahoo.com>
2016-12-11 19:25:27 +0100

    exercise 2.65 done

--- chapter2.scm
+++ chapter2.scm
@@ -741,12 +741,12 @@
 
 ;exercise 2.62
 (define (ord-union-set set1 set2)
-  (let ((x1 (car set1)) (x2 (car set2)))
-    (cond ((null? set1) set2)
-          ((null? set2) set1)
-          ((= x1 x2) (cons x1 (ord-union-set (cdr set1) (cdr set2))))
-          ((< x1 x2) (cons x1 (ord-union-set (cdr set1) set2)))
-          ((> x1 x2) (cons x2 (ord-union-set set1 (cdr set2)))))))
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else (let ((x1 (car set1)) (x2 (car set2)))
+                (cond ((= x1 x2) (cons x1 (ord-union-set (cdr set1) (cdr set2))))
+                      ((< x1 x2) (cons x1 (ord-union-set (cdr set1) set2)))
+                      ((> x1 x2) (cons x2 (ord-union-set set1 (cdr set2)))))))))
 
 (define (entry tree) (car tree))
 
@@ -832,3 +832,15 @@
 ; \  /\
 ; 3 7 11
 ;The order of growth for this procedure is O(n).
+
+;exercise 2.65
+(define (tr-union-set set1 set2)
+  (let ((ordset1 (tree->list-2 set1))
+        (ordset2 (tree->list-2 set2)))
+    (let ((ordset (ord-union-set ordset1 ordset2)))
+      (list->tree ordset))))
+(define (tr-intersection-set set1 set2)
+  (let ((ordset1 (tree->list-2 set1))
+        (ordset2 (tree->list-2 set2)))
+    (let ((ordset (ord-intersection-set ordset1 ordset2)))
+      (list->tree ordset))))
